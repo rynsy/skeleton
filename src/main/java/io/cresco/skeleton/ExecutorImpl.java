3be8a09a9efcf7fc48c35c3aad0a1c6090755ec4
Cody Bumgardner <cody@uky.edu>
2018-06-29 14:22:16 -0400

    getting closer

---
+++ src/main/java/io/cresco/skeleton/ExecutorImpl.java
@@ -0,0 +1,50 @@
+package io.cresco.skeleton;
+
+import io.cresco.library.messaging.MsgEvent;
+import io.cresco.library.plugin.Executor;
+import io.cresco.library.plugin.PluginBuilder;
+import io.cresco.library.utilities.CLogger;
+
+public class ExecutorImpl implements Executor {
+
+    private PluginBuilder plugin;
+    CLogger logger;
+
+    public ExecutorImpl(PluginBuilder pluginBuilder) {
+        this.plugin = pluginBuilder;
+        logger = plugin.getLogger(ExecutorImpl.class.getName(),CLogger.Level.Info);
+    }
+
+    @Override
+    public MsgEvent executeCONFIG(MsgEvent incoming) {
+        return null;
+    }
+    @Override
+    public MsgEvent executeDISCOVER(MsgEvent incoming) {
+        return null;
+    }
+    @Override
+    public MsgEvent executeERROR(MsgEvent incoming) {
+        return null;
+    }
+    @Override
+    public MsgEvent executeINFO(MsgEvent incoming) {
+        //logger.info("INCOMING INFO MESSAGE : " + incoming.getParams());
+        System.out.println("INCOMING INFO MESSAGE FOR PLUGIN");
+        return null;
+    }
+    @Override
+    public MsgEvent executeEXEC(MsgEvent incoming) {
+        return null;
+    }
+    @Override
+    public MsgEvent executeWATCHDOG(MsgEvent incoming) {
+        return null;
+    }
+    @Override
+    public MsgEvent executeKPI(MsgEvent incoming) {
+        return null;
+    }
+
+
+}
--- src/main/java/io/cresco/skeleton/Plugin.java
+++ src/main/java/io/cresco/skeleton/Plugin.java
@@ -3,6 +3,7 @@ package io.cresco.skeleton;
 
 import io.cresco.library.agent.AgentService;
 import io.cresco.library.messaging.MsgEvent;
+import io.cresco.library.plugin.Executor;
 import io.cresco.library.plugin.PluginBuilder;
 import io.cresco.library.plugin.PluginService;
 import org.osgi.framework.BundleContext;
@@ -22,21 +23,30 @@ public class Plugin implements PluginService {
 
     public BundleContext context;
     private PluginBuilder pluginBuilder;
+    private Executor executor;
 
     @Activate
     void activate(BundleContext context, Map<String,Object> map) {
 
         this.context = context;
 
-        System.out.println("Config Map PluginID:" + (String) map.get("pluginID"));
+
+        System.out.println("Started PluginID:" + (String) map.get("pluginID"));
 
         try {
             pluginBuilder = new PluginBuilder(this.getClass().getName(), context, map);
-            //pluginBuilder.getAgentService().getAgentState().sendMessage("AGENT: " + (String) map.get("pluginID"));
+            this.executor = new ExecutorImpl(pluginBuilder);
+            pluginBuilder.setExecutor(executor);
+
+            while(!pluginBuilder.getAgentService().getAgentState().isActive()) {
+                System.out.println("WAITING TO START!");
+                Thread.sleep(1000);
+            }
 
 
             MessageSender messageSender = new MessageSender(pluginBuilder);
             new Thread(messageSender).start();
+
 
         } catch(Exception ex) {
             ex.printStackTrace();
@@ -50,12 +60,8 @@ public class Plugin implements PluginService {
     }
 
     @Override
-    public boolean msgIn(String msg) {
-        return true;
-    }
-
-    @Override
-    public boolean msgIn(MsgEvent msg) {
+    public boolean inMsg(MsgEvent incoming) {
+        pluginBuilder.msgIn(incoming);
         return true;
     }
 
